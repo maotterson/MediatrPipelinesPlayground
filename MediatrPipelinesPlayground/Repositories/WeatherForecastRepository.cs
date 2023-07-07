2e87494ba1fe9315f2d98d76e580afc718826228
maotterson <otterson.2@gmail.com>
2023-07-07 01:48:55 -0400

    implement getweatherforecasts on repository

--- MediatrPipelinesPlayground/Repositories/WeatherForecastRepository.cs
+++ MediatrPipelinesPlayground/Repositories/WeatherForecastRepository.cs
@@ -2,4 +2,18 @@
 
 public class WeatherForecastRepository
 {
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+    public List<WeatherForecast> GetWeatherForecasts()
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateTime.Now.AddDays(index),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToList();
+    }
 }
