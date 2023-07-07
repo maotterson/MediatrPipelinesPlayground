71cdef4974016b0834e388b662963bec2a9ae29f
maotterson <otterson.2@gmail.com>
2023-07-07 01:59:00 -0400

    implement mediator pipeline on getweatherforecast controller method

--- MediatrPipelinesPlayground/Controllers/WeatherForecastController.cs
+++ MediatrPipelinesPlayground/Controllers/WeatherForecastController.cs
@@ -1,3 +1,5 @@
+using MediatR;
+using MediatrPipelinesPlayground.Contracts.GetWeatherForecast;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MediatrPipelinesPlayground.Controllers;
@@ -5,27 +7,21 @@ namespace MediatrPipelinesPlayground.Controllers;
 [Route("[controller]")]
 public class WeatherForecastController : ControllerBase
 {
-    private static readonly string[] Summaries = new[]
-    {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
 
     private readonly ILogger<WeatherForecastController> _logger;
+    private readonly IMediator _mediator;
 
-    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    public WeatherForecastController(
+        ILogger<WeatherForecastController> logger,
+        IMediator mediator)
     {
         _logger = logger;
+        _mediator = mediator;
     }
 
     [HttpGet(Name = "GetWeatherForecast")]
-    public IEnumerable<WeatherForecast> Get()
+    public async Task<ActionResult<GetWeatherForecastResponse>> Get()
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        {
-            Date = DateTime.Now.AddDays(index),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
+        var response = await _mediator.Send<GetWeatherForecastResponse>(new GetWeatherForecastRequest());
     }
 }
