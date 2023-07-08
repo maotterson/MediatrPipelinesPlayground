c8c8afa5d730c3cf077289a4477c18e54bf1c401
maotterson <otterson.2@gmail.com>
2023-07-08 01:26:01 -0400

    implement a forecast generator using .net default templates procedure

---
+++ MediatrPipelinesPlayground/Helpers/ForecastGenerator.cs
@@ -0,0 +1,21 @@
+﻿using MediatrPipelinesPlayground.Domain;
+
+namespace MediatrPipelinesPlayground.Helpers;
+
+public static class ForecastGenerator
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+    public static WeatherForecast GenerateWeatherForecast(DateTime generatedDate)
+    {
+        var forecast = new WeatherForecast
+        {
+            Date = generatedDate,
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        };
+        return forecast;
+    }
+}
