da0458ea13cc7c4beac4d0c0d380bb28b11707ef
maotterson <otterson.2@gmail.com>
2023-07-08 00:47:50 -0400

    create basic weatherdbcontext with single dbset of weatherforecasts

--- MediatrPipelinesPlayground/Database/WeatherDbContext.cs
+++ MediatrPipelinesPlayground/Database/WeatherDbContext.cs
@@ -1,7 +1,13 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using MediatrPipelinesPlayground.Domain;
+using Microsoft.EntityFrameworkCore;
 
 namespace MediatrPipelinesPlayground.Database;
 
 public class WeatherDbContext : DbContext
 {
+    public DbSet<WeatherForecast> WeatherForecasts { get; set; }
+    public WeatherDbContext(DbContextOptions<WeatherDbContext> options) : base(options)
+    {
+    }
+
 }
--- MediatrPipelinesPlayground/Repositories/WeatherForecastPostgresRepository.cs
+++ MediatrPipelinesPlayground/Repositories/WeatherForecastPostgresRepository.cs
@@ -1,9 +1,15 @@
-﻿using MediatrPipelinesPlayground.Domain;
+﻿using MediatrPipelinesPlayground.Database;
+using MediatrPipelinesPlayground.Domain;
 
 namespace MediatrPipelinesPlayground.Repositories;
 
 public class WeatherForecastPostgresRepository : IWeatherForecastRepository
 {
+    private readonly WeatherDbContext _weatherDbContext;
+    public WeatherForecastPostgresRepository(WeatherDbContext weatherDbContext)
+    {
+        _weatherDbContext = weatherDbContext;
+    }
     public Task<List<WeatherForecast>> GetWeatherForecasts()
     {
         throw new NotImplementedException();
