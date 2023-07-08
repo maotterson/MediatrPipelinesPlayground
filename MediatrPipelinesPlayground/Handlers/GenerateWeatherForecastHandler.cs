a94daafd0167817b95a6faf309b1723a28efacb6
maotterson <otterson.2@gmail.com>
2023-07-08 01:38:31 -0400

    implement handle on generateweatherforecasthandler

--- MediatrPipelinesPlayground/Handlers/GenerateWeatherForecastHandler.cs
+++ MediatrPipelinesPlayground/Handlers/GenerateWeatherForecastHandler.cs
@@ -1,9 +1,20 @@
 ﻿using MediatR;
 using MediatrPipelinesPlayground.Contracts.GenerateWeatherForecast;
 using MediatrPipelinesPlayground.Contracts.GetWeatherForecast;
+using MediatrPipelinesPlayground.Repositories;
 
 namespace MediatrPipelinesPlayground.Handlers;
 
 public class GenerateWeatherForecastHandler : IRequestHandler<GenerateWeatherForecastRequest, GenerateWeatherForecastResponse>
 {
+    private readonly IWeatherForecastRepository _weatherForecastRepository;
+    public GenerateWeatherForecastHandler(IWeatherForecastRepository weatherForecastRepository)
+    {
+        _weatherForecastRepository = weatherForecastRepository;
+    }
+    public async Task<GenerateWeatherForecastResponse> Handle(GenerateWeatherForecastRequest request, CancellationToken cancellationToken)
+    {
+        var weatherForecast = await _weatherForecastRepository.GenerateWeatherForecast();
+        return weatherForecast.ToDto();
+    }
 }
