9f86ca6daf1a8e1dae5a775dae334031d3a048e2
maotterson <otterson.2@gmail.com>
2023-07-08 17:45:29 -0400

    create simple logging behavior implementation

--- MediatrPipelinesPlayground/Pipelines/LoggingBehavior.cs
+++ MediatrPipelinesPlayground/Pipelines/LoggingBehavior.cs
@@ -4,4 +4,16 @@ namespace MediatrPipelinesPlayground.Pipelines;
 
 public class LoggingBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : class, IRequest<TResponse>
 {
+    private readonly ILogger<LoggingBehavior<TRequest, TResponse>> _logger;
+    public LoggingBehavior(ILogger<LoggingBehavior<TRequest, TResponse>> logger)
+    {
+        _logger = logger;
+    }
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        _logger.LogInformation($"{typeof(TRequest).Name} handled");
+        var response = await next();
+        _logger.LogInformation($"{typeof(TResponse).Name} created");
+        return response;
+    }
 }
