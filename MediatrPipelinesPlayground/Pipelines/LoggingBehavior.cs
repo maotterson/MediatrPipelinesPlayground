ec1aa2739852c587864786914f9a80f3ccb96b6b
maotterson <otterson.2@gmail.com>
2023-07-08 17:37:35 -0400

    create logging behavior class contract

---
+++ MediatrPipelinesPlayground/Pipelines/LoggingBehavior.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+
+namespace MediatrPipelinesPlayground.Pipelines;
+
+public class LoggingBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : class, IRequest<TResponse>
+{
+}
