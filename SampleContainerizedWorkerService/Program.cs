29156739f73eccd1c7fc9179b0e59cc186a22909
maotterson <otterson.2@gmail.com>
2023-07-08 18:13:08 -0400

    add env var for worker service to serve reference to app api

--- SampleContainerizedWorkerService/Program.cs
+++ SampleContainerizedWorkerService/Program.cs
@@ -3,6 +3,7 @@ using SampleContainerizedWorkerService;
 IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices(services =>
     {
+        var appUrl = Environment.GetEnvironmentVariable("appUrl");
         services.AddHostedService<Worker>();
     })
     .Build();
